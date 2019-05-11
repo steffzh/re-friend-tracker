33598cdc651c4e885fddd5f7629621f87aecfb07
steffzh <50523213+steffzh@users.noreply.github.com>
2019-05-11 17:19:30 +0200

    Update guimodel.ts

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Steff's Engineering Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
